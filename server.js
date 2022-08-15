7e4c487d1ce020125d3c7a2a6c54c50d8542d0b7
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-15 04:48:32 +0200

    finished login

--- server.js
+++ server.js
@@ -1,12 +1,64 @@
 import express from 'express';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = 3024;
+
+app.use(
+	session({
+		resave: true,
+		saveUninitialized: true,
+		secret: 'werwyerusj234234hskjdfh',
+	})
+);
+app.use(cookieParser());
+
+const users = [
+	{
+		username: 'ja',
+		firstName: 'Jörg',
+		lastName: 'Ackermann',
+		email: 'ja@mail.com',
+	},
+	{
+		username: 'ac',
+		firstName: 'Angelika',
+		lastName: 'Carstense',
+		email: 'ac@mail.com',
+	},
+];
 
 app.get('/', (req, res) => {
 	res.send('session/cookie basic test');
 });
 
+app.get('/login/:username', (req, res) => {
+	const user = users.find((user) => user.username === req.params.username);
+	if (user) {
+		req.session.user = user;
+		req.session.cookie.expires = new Date(Date.now() + 10000); // 10 seconds
+		req.session.save();
+		res.send(`User logged in: ${JSON.stringify(user)}`);
+	} else {
+		res.status(500).send('bad login');
+	}
+});
+
+app.get('/current-user', (req, res) => {
+	if (req.session.user) {
+		res.send(req.session.user);
+	} else {
+		res.send('no user logged in');
+	}
+});
+
+app.get('/logout', (req, res) => {
+	req.session.destroy((err) => {
+		res.send('User logged out');
+	});
+});
+
 app.listen(PORT, () => {
-	console.log(`listening at http://localhost:${PORT}`)
+	console.log(`listening at http://localhost:${PORT}`);
 });
